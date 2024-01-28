caf676421de5b45558b5857fc5e4786a24d2e669
YONGHWA\ekore <yonghwa95@naver.com>
2024-01-29 08:44:38 +0900

    파라미터 기본값 지정

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -58,4 +58,14 @@ public class RequestParamController {
         log.info("username = {}, age = {}", username, age);
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamRequired (
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age
+    ) {
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
 }
